9ab97ac174cafe2df4054bb9d7a03c71c61828e9
Edgar Torres <edgarlb28@outlook.com>
2025-09-13 09:11:10 -0600

    creacion de arquitectura iniciaal

---
+++ Models/GlobalData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.ObjectModel;
+
+namespace AppointmentSimulator.Models
+{
+    public static class GlobalData
+    {
+        public static ObservableCollection<Appointment> Appointments { get; } = [
+            new Appointment
+            {
+                Name = "Alejandro Barroeta",
+                Subject = "Cambio de carrera",
+                AppointmentDate = new DateOnly(2025,09,14),
+                StartingTime = new TimeSpan(8,0,0),
+                EndingTime = new TimeSpan(8,30,0)
+            }
+        ];
+    }
+}
---
+++ Pages/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using AppointmentSimulator.Models;
+
+namespace AppointmentSimulator.Pages
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            AppointmentsCollectionView.ItemsSource = GlobalData.Appointments;
+        }
+
+    }
+}
